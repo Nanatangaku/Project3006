88b374e0ead42ce6b0693652a74216236522429a
NanaTengoku <ku123912399asb@gmail.com>
2024-04-24 19:36:46 +0800

    finish index

---
+++ js/rotation.js
@@ -0,0 +1,58 @@
+let left = document.querySelector(".button-left")
+let right = document.querySelector(".button-right")
+let m = document.querySelectorAll(".m")
+let images = document.querySelector(".images")
+let index = 0
+
+function position() {
+    images.style.left = (index * -100) + "%"
+}
+
+function add() {
+    if (index >= m.length - 1) {
+        index = 0
+    } else {
+        index++
+    }
+}
+
+function desc() {
+    if (index < 1) {
+        index = m.length - 1
+    } else {
+        index--
+    }
+}
+
+function timer() {
+    time = setInterval(() => {
+        index++
+        desc()
+        add()
+        position()
+    }, 3000)
+}
+
+left.addEventListener("click", () => {
+    desc()
+    position()
+    clearInterval(time)
+    timer()
+})
+
+right.addEventListener("click", () => {
+    add()
+    position()
+    clearInterval(time)
+    timer()
+})
+
+for (let i = 0; i < m.length; i++) {
+    m[i].addEventListener("click", () => {
+        index = i;
+        position();
+        clearInterval(time)
+        timer()
+    })
+}
+
