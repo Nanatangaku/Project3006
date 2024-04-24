55c0ee72e0a89b1f3b1c7a01dd6efa2058505be1
NanaTengoku <ku123912399asb@gmail.com>
2024-04-24 21:03:54 +0800

    last for index page

--- js/main.js
+++ js/main.js
@@ -57,3 +57,19 @@ function instagram() {
 function tiktok() {
     window.open("https://tiktok.com");
 }
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.bottom < 0 || rect.top > window.innerHeight;
+}
+
+function addClassToVisibleElements() {
+    var aosElements = document.querySelectorAll(".aos");
+    aosElements.forEach(function (aosElement) {
+        if (!isElementInViewport(aosElement)) aosElement.classList.add("ed");
+        else aosElement.classList.remove("ed");
+    });
+}
+
+document.addEventListener("scroll", addClassToVisibleElements);
+addClassToVisibleElements();
