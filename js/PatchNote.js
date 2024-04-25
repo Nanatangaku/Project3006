28a78e08cd0db07d4747c12bd7be12251262ada6
NanaTengoku <ku123912399asb@gmail.com>
2024-04-25 21:03:20 +0800

    finish page patchnote

---
+++ js/PatchNote.js
@@ -0,0 +1,19 @@
+function sectionOne_lineOne__left() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-8-notes/");
+}
+function sectionOne_lineOne__mid() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-7-notes/");
+}
+function sectionOne_lineOne__right() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-6-notes/");
+}
+
+function sectionOne_lineTwo__left() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-5-notes/");
+}
+function sectionOne_lineTwo__mid() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-4-notes/");
+}
+function sectionOne_lineTwo__right() {
+    window.open("https://www.leagueoflegends.com/en-gb/news/game-updates/patch-14-3-notes/");
+}
