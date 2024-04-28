a6e077ff1dcbbebf0673311c863161b6b7f4ef3c
LogRL <jack1242003@gmail.com>
2024-04-29 02:29:28 +0800

    fix bug
    29/4 2:29

--- js/login.js
+++ js/login.js
@@ -1,7 +1,12 @@
 var x = document.getElementById("login");
 var y = document.getElementById("register");
 
-function login() {
+function login(){
+    x.style.left = "4px";
+    y.style.right = "520px";
+}
+
+function backtologin(){
     x.style.left = "4px";
     y.style.right = "520px";
 }
@@ -9,4 +14,7 @@ function login() {
 function register() {
     x.style.left = "-510px";
     y.style.right = "4px";
+}
+function tohome() {
+    window.location.href = "index.html";
 }
